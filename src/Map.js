aa93ba9f7171e51fefa192e493679fb482ee1460
@dianavile <diana.vile@gmail.com>
2018-12-19 15:13:51 +0100

    Create Map.js
    Create empty Map component

---
+++ src/Map.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import './App.css';
+
+//Map Component
+class Map extends Component {
+
+
+render() {
+    return (
+        <GoogleMap></GoogleMap>
+    );
+    }
+
+}
+
+export default Map;

efbdadf1ba26f3429a8aa1cf6c833beaf8de8121
@dianavile <diana.vile@gmail.com>
2018-12-19 15:37:34 +0100

    Add empty locations.js file
    Create locations component

---
+++ src/Locations.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import './App.css';
+
+class Locations extends Component {
+    state = {   
+    }
+
+     render() {
+        return 
+    }
+}
+
+export default Locations;  

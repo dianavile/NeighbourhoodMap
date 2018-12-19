3bc1fffa71f0adbe3f1e47ea91dae1715dc2363c
@dianavile <diana.vile@gmail.com>
2018-12-19 15:30:42 +0100

    uncomment Logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import Header from './Header.js';
 import Map from './Map.js';

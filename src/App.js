f9ff139b42cbf96c2ce3d6799f394c638efd2fec
@dianavile <diana.vile@gmail.com>
2018-12-19 14:45:44 +0100

    Update App.js
    Import Footer component from Footer.js
    add Footer to Render- Return to show Footer on page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './Header.js';
+import Footer from './Footer.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
          <Header />
+         <Footer />
       </div>
     );
   }

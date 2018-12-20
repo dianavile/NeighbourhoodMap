c8b45a6a5b90d1d8cd517679f1a6fe7526acf7eb
@dianavile <diana.vile@gmail.com>
2018-12-20 11:42:44 +0100

    Update Map.js
    Update Map component with:
    - import mapStyle from "./mapStyle";
    - import { withGoogleMap, GoogleMap, Marker } from "react-google-maps";
    - import InfoBox from "react-google-maps/lib/components/addons/InfoBox";
    - const google = window.google;
    
    Render Map Component to return:
    - GoogleMap
    - Marker
    - Infowindow

--- src/Map.js
+++ src/Map.js
@@ -1,16 +1,45 @@
 import React, {Component} from 'react';
 import './App.css';
+import mapStyle from "./mapStyle";
+import { withGoogleMap, GoogleMap, Marker } from "react-google-maps";
+import InfoBox from "react-google-maps/lib/components/addons/InfoBox";
+const google = window.google;
+
 
 //Map Component
 class Map extends Component {
 
+        state = {
+            mapstyle: mapStyle,
+        }
 
-render() {
-    return (
-        <GoogleMap></GoogleMap>
-    );
-    }
+    render() {
+        return (
+            <GoogleMap
+                defaultZoom={8}
+                defaultCenter={{ lat: 41.3851, lng: 2.1734}}
+                defaultOptions={{ styles: this.state.mapstyle }}
+                zoom={this.props.zoom}
+                center={{ lat: this.props.lat, lng: this.props.lng}}
+            >
+                {this.props.venues.length && this.props.venues.map((place, index) => (
+                    <Marker key={place.venue.id} position={{lat: place.venue.location.lat, lng: place.venue.location.lng}} animation={place.verified ? window.google.maps.Animation.BOUNCE : null} onClick={() => this.props.toggleInfoBox(place.venue.id)}>
+                        <div>
+                            <InfoBox options={{pixelOffset: new google.maps.Size(-120, -200) , closeBoxURL: '',boxStyle: {width: "200px"} }} visible={place.verified}>
+                                <div>
+                                    <h2>{place.venue.name}</h2>
+                                    <p>{place.venue.location.formattedAddress[0]}</p>
+                                </div>
+                            </InfoBox>
+                        </div>
+                    </Marker>
+                    )
+                )
+                }
+            </GoogleMap>
+        );
+        }
 
 }
 
-export default Map;
+export default withGoogleMap(Map);

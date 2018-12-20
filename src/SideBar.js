85b6ed9b536e4e28e4054054b407404e35df0265
@dianavile <diana.vile@gmail.com>
2018-12-20 11:44:41 +0100

    Create Sidebar.js

---
+++ src/SideBar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class SideBar extends Component {
+    render() {
+        return (
+            <aside id='aside' tabIndex='0'>
+            <div className="SideBar">Add SideBar here</div>
+          </aside >
+        );
+    }
+}
+
+export default SideBar;

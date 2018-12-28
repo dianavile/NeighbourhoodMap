105d10a27ba58b49b765ab070633fb27a751f131
@dianavile <diana.vile@gmail.com>
2018-12-28 09:50:56 +0100

    Add production mode
    Upload build folder to add production mode

---
+++ build/precache-manifest.8f5b9628d5904e359e16daa641a7be90.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0e7dfe7bf95dcf8f7207",
+    "url": "/static/js/main.0e7dfe7b.chunk.js"
+  },
+  {
+    "revision": "03cfef2cf5ad0b873cc3",
+    "url": "/static/js/1.03cfef2c.chunk.js"
+  },
+  {
+    "revision": "0e7dfe7bf95dcf8f7207",
+    "url": "/static/css/main.231ddcfd.chunk.css"
+  },
+  {
+    "revision": "f9bcd3acc002cf94b90327e2a95c144f",
+    "url": "/index.html"
+  }
+];
